cb92f8031b76c2bd713ed299f20c941e29e64630
Ahmad Mohammad Haggag <96918526+ahmad-haggag82@users.noreply.github.com>
2024-10-16 03:19:01 +0300

    Update test_dag_run.sh

--- tests/test_dag_run.sh
+++ tests/test_dag_run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-git diff --name-only $COMMIT_ID >> dag_out
-cat dag_out
-grep -E "dags/config|dags/sql" dag_out | awk -F / '{print $3}' | sort | uniq | while IFS= read -r dir; do
+git diff --name-only $COMMIT_ID | grep -E "dags/config|dags/sql" | awk -F / '{print $3}' | sort | uniq | while IFS= read -r dir; do
   echo "Dag config directory name: $dir"
   dag_name=$(grep -m1 'name:' config/$dir/*.yaml | awk '{ print $2}')
   echo "Dag Name : $dag_name"
